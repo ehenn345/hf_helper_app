4221214d3c3d9a36157d36f7293e1797d99852f1
Erin Weston <westonerin1@gmail.com>
2020-12-22 10:39:33 -0500

    create migration to change user password to password digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_20_215206) do
+ActiveRecord::Schema.define(version: 2020_12_21_161136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,11 +34,11 @@ ActiveRecord::Schema.define(version: 2020_12_20_215206) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.integer "sodium_limit"
     t.integer "fluid_limit"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   create_table "weights", force: :cascade do |t|
