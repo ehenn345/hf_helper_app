f4bbdd6a84bc88e6db4bc06c60685dec8da89e07
Erin Weston <westonerin1@gmail.com>
2021-01-02 17:02:39 -0500

    hard code sodium daily total method

--- app/models/sodium.rb
+++ app/models/sodium.rb
@@ -1,3 +1,15 @@
 class Sodium < ApplicationRecord
   belongs_to :user
+
+  def sodium_daily_total
+    sodiums_for_day = user.sodia.where({date: "2020-12-01"})
+    p sodiums_for_day
+    total_sodium_for_day = 0
+    sodiums_for_day.each do |sodium_for_day|
+      total_sodium_for_day += sodium_for_day[:input_sodium]
+    end
+    p "Total sodium"
+    p total_sodium_for_day
+  end
+
 end
